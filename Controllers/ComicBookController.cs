59c15659c373aa7691ff5071394c80a9c56556e1
alextalmazan <alextalmazan1996@gmail.com>
2019-09-15 00:08:14 +0100

    Add project files.

---
+++ Controllers/ComicBookController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComicBookGalery.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Detail()
+        {
+            return "Hello from the comic books controller";
+        }
+    }
+}
