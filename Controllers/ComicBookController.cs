ec7cef2f1b6e19d616c8b5234e0486d80d97aea3
alextalmazan <alextalmazan1996@gmail.com>
2019-09-15 15:17:26 +0100

    Updated Commic Book Detail View

--- Controllers/ComicBookController.cs
+++ Controllers/ComicBookController.cs
@@ -21,6 +21,17 @@ namespace ComicBookGalery.Controllers
 
         public IActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: dan slott",
+                "Pencils: humberto ramos",
+                "Links: victor olazaba",
+                "Colors: edgar delgado",
+                "Letters: chris eliopoulos"
+    };
             return View();
         }
     }
